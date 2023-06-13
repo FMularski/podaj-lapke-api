1f0fc584bee894b460f03cc74ea5b3dc9b959c32
Filip <filipelblag@gmail.com>
2023-06-13 22:25:52 +0200

    user model admin page

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,33 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "phone")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (
+            _("Notifications"),
+            {"fields": ("notification_push", "notification_email", "notification_favourites")},
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
