29aa0d5aba3abdd3e2423cd727e7bdca275d9091
Filip <filipelblag@gmail.com>
2023-06-13 23:29:51 +0200

    user serialzier password write only

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -17,6 +17,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             "password",
         )
         read_only_fields = ("id",)
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
